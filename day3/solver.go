ec19037b0e1b5a237ea9af3cd8d8a799ae1b1058
Ozan <ozancaglar1996@hotmail.co.uk>
2023-12-03 14:44:07 +0000

    get digits in row

--- day3/solver.go
+++ day3/solver.go
@@ -3,6 +3,7 @@ package day3
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"unicode"
 
 	"github.com/ozancaglar/advent-of-code-2023/util"
@@ -33,4 +34,46 @@ func getSymbolCol(input string) (cols []int) {
 	}
 
 	return symbolColumns
+}
+
+func getDigitsInRow(row string) []rowNumber {
+	rn := make([]rowNumber, 0)
+	toContinue := 0
+	for i, c := range row {
+		if toContinue != 0 {
+			toContinue -= 1
+			continue
+		}
+
+		if unicode.IsNumber(c) {
+			for j, k := range row[i:] {
+				endOfRow := unicode.IsNumber(k) && i+j == len(row)-1
+				if !unicode.IsNumber(k) || endOfRow {
+					endIndex := i + j
+					if endOfRow {
+						endIndex += 1
+					}
+					number, err := strconv.Atoi(row[i:endIndex])
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					indexes := make([]int, 0)
+					for k := i; k < endIndex; k++ {
+						indexes = append(indexes, k)
+					}
+					rn = append(rn, rowNumber{number: number, indexes: indexes})
+					toContinue = j
+					break
+				}
+			}
+		}
+	}
+	return rn
+}
+
+type rowNumber struct {
+	number  int
+	indexes []int
+	added   bool
 }
--- day3/solver_test.go
+++ day3/solver_test.go
@@ -11,3 +11,10 @@ func TestGetSymbolCol(t *testing.T) {
 	assert.Equal(t, []int{}, getSymbolCol(".........."))
 	assert.Equal(t, []int{2, 9}, getSymbolCol("..*......*"))
 }
+
+func TestGetDigitsInRow(t *testing.T) {
+	assert.Equal(t, []rowNumber{}, getDigitsInRow("...*......"))
+	assert.Equal(t, []rowNumber{{number: 35, indexes: []int{2, 3}, added: false}, {number: 633, indexes: []int{6, 7, 8}, added: false}}, getDigitsInRow("..35..633."))
+	assert.Equal(t, []rowNumber{{number: 617, indexes: []int{0, 1, 2}, added: false}}, getDigitsInRow("617*......"))
+	assert.Equal(t, []rowNumber{{number: 617, indexes: []int{0, 1, 2}, added: false}, {number: 617, indexes: []int{7, 8, 9}, added: false}}, getDigitsInRow("617*...617"))
+}
