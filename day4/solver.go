4f025e17461d85d279099e64a99621904078cdf1
Ozan <ozancaglar1996@hotmail.co.uk>
2023-12-04 15:07:23 +0000

    p1 and tidy

--- day4/solver.go
+++ day4/solver.go
@@ -1,7 +1,6 @@
 package day4
 
 import (
-	"fmt"
 	"log"
 	"math"
 	"regexp"
@@ -15,21 +14,37 @@ func Solve(filename string) {
 	scanner := util.StreamLines(filename)
 	re := regexp.MustCompile("\\d+")
 	cards := []string{}
-	parsedCards := make([]*card, 0)
-	winningNumbers := make(map[int]bool)
 
+	winningNumbers := make(map[int][]int)
+
+	j := 0
 	for scanner.Scan() {
 		splitString := strings.Split(scanner.Text(), "|")
 		cards = append(cards, splitString[1])
-		for _, n := range re.FindAllString(splitString[0], -1) {
+		winningNumbersInRow := re.FindAllString(splitString[0], -1)[1:]
+		winningNumbersInRowAsInt := make([]int, 0)
+		for _, n := range winningNumbersInRow {
 			i, err := strconv.Atoi(n)
 			if err != nil {
 				log.Fatal(err)
 			}
-			winningNumbers[i] = true
+			winningNumbersInRowAsInt = append(winningNumbersInRowAsInt, i)
 		}
+		winningNumbers[j] = winningNumbersInRowAsInt
+		j += 1
 	}
 
+	log.Printf("Day four, part one answer: %v", sumForPartOne(parseCards(cards, re), winningNumbers))
+}
+
+type card struct {
+	id                     int
+	numberOfWinningNumbers int
+	numbers                []int
+}
+
+func parseCards(cards []string, re *regexp.Regexp) []*card {
+	parsedCards := make([]*card, 0)
 	for cardNumber, c := range cards {
 		numbers := make([]int, 0)
 		for _, n := range re.FindAllString(c, -1) {
@@ -41,25 +56,22 @@ func Solve(filename string) {
 		}
 		parsedCards = append(parsedCards, util.ToPtr(card{id: cardNumber + 1, numbers: numbers}))
 	}
+	return parsedCards
+}
+
+func sumForPartOne(parsedCards []*card, winningNumbers map[int][]int) int {
 	sum := 0
-	for _, c := range parsedCards {
-		for _, n := range c.numbers {
-			_, ok := winningNumbers[n]
-			if ok {
-				c.numberOfWinningNumbers += 1
+	for i, c := range parsedCards {
+		for _, v := range winningNumbers[i] {
+			for _, number := range c.numbers {
+				if v == number {
+					c.numberOfWinningNumbers += 1
+				}
 			}
 		}
-		fmt.Println(c)
-		fmt.Println(c.numberOfWinningNumbers)
 		if c.numberOfWinningNumbers > 0 {
 			sum += int(math.Pow(2, float64(c.numberOfWinningNumbers)-1))
 		}
 	}
-	fmt.Println(sum)
-}
-
-type card struct {
-	id                     int
-	numberOfWinningNumbers int
-	numbers                []int
+	return sum
 }
